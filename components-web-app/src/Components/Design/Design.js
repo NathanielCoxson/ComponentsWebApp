d42c8bd0753756df38efd598afe3c495be99227b
NathanCoxson <nathancoxson2739@gmail.com>
2023-09-18 11:48:24 -0400

    Set up tailwind and wrote sample styles with it.

---
+++ components-web-app/src/Components/Design/Design.js
@@ -0,0 +1,9 @@
+
+
+export default function Design() {
+    return (
+        <div className="w-full bg-slate">
+            <h2 className="text-white text-3xl">Gradient</h2>
+        </div>
+    )
+}
---
+++ components-web-app/src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+
+
+export default function Navbar() {
+    return (
+        <>
+            <div className="box-border bg-slate-900 h-14 w-full">
+                
+            </div>
+        </>
+    )
+}
